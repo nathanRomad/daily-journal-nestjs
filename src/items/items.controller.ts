5675600680bf6a324e2a368f8d435c796cba8511
Nathan Hamilton <nathan.a.hamilton@gmail.com>
2021-09-10 12:26:09 -0500

    build: mock data

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param } from '@nestjs/common';
+import { }
+import { ItemsService } from './items.service';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    constructor(private readonly issuesService: ItemsService) { }
+
+    @Get()
+    async findAll() {
+        const issues = await this.issuesService.findAll();
+        return issues;
+    }
+
+    // @Get(':id')
+    // async findOne(@Param('id') id) {
+    //     const issue = await this.issuesService.findOne(id);
+    //     return issue;
+    // }
+
+    // @Post()
+    // async create(@Body() createIssueDto: CreateIssueDto) {
+    //     const issue = await this.issuesService.create(createIssueDto)
+    //     return issue;
+    // }
+
+    // //endpoint for delete is /issues/?issueId=4
+    // @Delete()
+    // async delete(@Query() query) {
+    //     const issues = await this.issuesService.delete(query.issueId);
+    //     return issues;
+    // }
+
+
+    // @Put(':id')
+    // async update(@Body() createIssueDto: CreateIssueDto, @Param('id') id) {
+    //     const issues = this.issuesService.update(createIssueDto, id);
+    //     return issues
+    // }
+
+}
---
+++ src/items/items.mock.ts
@@ -0,0 +1,12 @@
+export const ITEMS = [
+  {
+    id: 1,
+    title: 'One',
+    description: 'description',
+  },
+  {
+    id: 2,
+    title: 'Two',
+    description: 'description2',
+  },
+];
