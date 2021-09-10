9784f33bdc029f451a6508576857861f3433e31a
Nathan Hamilton <nathan.a.hamilton@gmail.com>
2021-09-10 13:04:33 -0500

    build: added mock data to controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,40 +1,46 @@
-import { Controller, Get, Post, Put, Patch, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
-    constructor(private readonly issuesService: ItemsService) { }
+  constructor(private readonly issuesService: ItemsService) {}
 
-    @Get()
-    async findAll() {
-        const issues = await this.issuesService.findAll();
-        return issues;
-    }
+  @Get()
+  async findAll() {
+    const issues = await this.issuesService.findAll();
+    return issues;
+  }
 
-    // @Get(':id')
-    // async findOne(@Param('id') id) {
-    //     const issue = await this.issuesService.findOne(id);
-    //     return issue;
-    // }
+  // @Get(':id')
+  // async findOne(@Param('id') id) {
+  //     const issue = await this.issuesService.findOne(id);
+  //     return issue;
+  // }
 
-    // @Post()
-    // async create(@Body() createIssueDto: CreateIssueDto) {
-    //     const issue = await this.issuesService.create(createIssueDto)
-    //     return issue;
-    // }
+  // @Post()
+  // async create(@Body() createIssueDto: CreateIssueDto) {
+  //     const issue = await this.issuesService.create(createIssueDto)
+  //     return issue;
+  // }
 
-    // //endpoint for delete is /issues/?issueId=4
-    // @Delete()
-    // async delete(@Query() query) {
-    //     const issues = await this.issuesService.delete(query.issueId);
-    //     return issues;
-    // }
+  // //endpoint for delete is /issues/?issueId=4
+  // @Delete()
+  // async delete(@Query() query) {
+  //     const issues = await this.issuesService.delete(query.issueId);
+  //     return issues;
+  // }
 
-
-    // @Put(':id')
-    // async update(@Body() createIssueDto: CreateIssueDto, @Param('id') id) {
-    //     const issues = this.issuesService.update(createIssueDto, id);
-    //     return issues
-    // }
-
+  // @Put(':id')
+  // async update(@Body() createIssueDto: CreateIssueDto, @Param('id') id) {
+  //     const issues = this.issuesService.update(createIssueDto, id);
+  //     return issues
+  // }
 }
